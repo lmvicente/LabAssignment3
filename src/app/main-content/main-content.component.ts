da65d09fa73a295d6c9cd00f2dd3a7c7481d89a6
lmvicente <leah.m.vicente@gmail.com>
2018-10-17 01:06:17 -0700

    First Commit

---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+interface IStudent {
+  id: number;
+  firstName: string;
+  lastName: string;
+  course: string;
+}
+
+@Component({
+  selector: 'main-content',
+  templateUrl: './main-content.component.html',
+  styleUrls: ['./main-content.component.css']
+})
+export class MainContentComponent implements OnInit {
+
+
+  students: Array<IStudent> = [];
+
+  constructor() {
+    this.students[0] = {
+      id: 1,
+      firstName: 'Leah',
+      lastName: 'Vicente',
+      course: 'Business'
+    };
+
+    this.students[1] = {
+      id: 2,
+      firstName: 'John',
+      lastName: 'Smith',
+      course: 'Programming'
+    };
+
+    this.students[2] = {
+      id: 3,
+      firstName: 'Jane',
+      lastName: 'Doe',
+      course: 'Math'
+    };
+
+    this.students[3] = {
+      id: 4,
+      firstName: 'Laura',
+      lastName: 'Carter',
+      course: 'Theatre'
+    };
+  }
+
+  ngOnInit() {
+  }
+
+  addStudent() {
+  const student: IStudent = {
+    id: 5,
+    firstName: 'Joe',
+    lastName: 'Biden',
+    course: 'Politics'
+  };
+
+    this.students.push();
+  }
+}
